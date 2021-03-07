2e0ebc00387d764cdeebd0df9814a6e802d463e8
Tim Honermann <honermti@windowslive.com>
2021-03-07 01:07:44 +0100

    updated docs and vue.config

--- vue.config.js
+++ vue.config.js
@@ -4,8 +4,5 @@
  * @type {import('@vue/cli-service').ProjectOptions}
  */
 module.exports = {
-  publicPath:
-    process.env.NODE_ENV === "production"
-      ? "/docs" // This is whatever your path from the root is
-      : "/"
+  publicPath: "/"
 };
